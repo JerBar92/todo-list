d2c162e915d01a8160f3c60722465d6dc6c977f8
JerBar92 <174113693+JerBar92@users.noreply.github.com>
2024-10-15 18:12:48 +0200

    Modify project.js and delete title and button for displayProject

--- src/index.js
+++ src/index.js
@@ -11,5 +11,18 @@ const newTaskBtn = document.querySelector("#newTaskBtn");
 const newTask = document.querySelector("#newTask");
 const cancelTask = document.querySelector("#cancelTask");
 
-newTaskBtn?.addEventListener("click", () => newTask.show());
-cancelTask?.addEventListener("click", () => newTask.close());
+newTaskBtn?.addEventListener("click", () => (newTask.style.display = "block"));
+cancelTask?.addEventListener("click", () => (newTask.style.display = "none"));
+
+const projectTitle = document.querySelector("#projectTitle");
+const addProject = document.querySelector("#addProject");
+
+project.displayProjectList("My Project", []);
+
+addProject?.addEventListener("click", (event) => {
+  event.preventDefault();
+  project.displayProjectList(projectTitle?.value, []);
+  newProject?.close();
+});
+
+const projectDiv = document.querySelector(`#projectDiv${index}`);
--- src/project.js
+++ src/project.js
@@ -1,9 +1,55 @@
 export const project = (function () {
-  function createProject(title, toDo) {
+  const projectArray = [];
+
+  function projects(title, toDo) {
     return {
       title: title,
       toDo: toDo,
     };
   }
-  return { createProject };
+
+  const listProjects = () => projectArray;
+
+  const createProject = () => {
+    const projectList = document.querySelector("#projectList");
+    const projectDiv = document.createElement("div");
+    const projectAdd = document.createElement("li");
+    const projectRemove = document.createElement("button");
+    projectArray.forEach((project, index) => {
+      projectAdd.textContent = project.title;
+      projectRemove.textContent = "Remove";
+      projectRemove.id = `projectRemove${index}`;
+      projectDiv.id = `projectDiv${index}`;
+      projectDiv.appendChild(projectAdd);
+      projectDiv.appendChild(projectRemove);
+      projectList?.appendChild(projectDiv);
+
+      /*projectRemove.addEventListener("click", () => {
+        projectArray.splice(index, 1);
+      });*/
+    });
+    console.log(projectArray);
+  };
+
+  const displayProjectList = (title, toDo) => {
+    const newProject = projects(title, toDo);
+    projectArray.push(newProject);
+    createProject();
+  };
+
+  const displayProject = (title) => {
+    const displayProject = document.querySelector("#displayProject");
+    const projectHead = document.createElement("div");
+    const projectName = document.createElement("h2");
+    const newTask = document.createElement("button");
+    projectHead.id = "projectHead";
+    projectName.id = "projectName";
+    projectName.textContent = title;
+    projectHead.appendChild(projectName);
+    newTask.id = "newTaskBtn";
+    projectHead.appendChild(newTask);
+    displayProject?.appendChild(projectHead);
+  };
+
+  return { listProjects, displayProjectList, displayProject };
 })();
